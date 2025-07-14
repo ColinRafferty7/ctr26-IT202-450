807ce900162ee35febfce9438565d4bc70c2b211
ColinRafferty7 <ctr6mince@gmail.com>
2025-07-13 21:06:52 -0400

    updating ucid for problem 1 oops

--- public_html/M6/problem1.php
+++ public_html/M6/problem1.php
@@ -49,7 +49,7 @@ function processBirds($birds) {
     echo "<pre>" . var_export($subset, true) . "</pre>";
    
 }
-$ucid = "mt85"; // replace with your UCID
+$ucid = "ctr26"; // replace with your UCID
 printHeader($ucid, 1); 
 ?>
 <table>
